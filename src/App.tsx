d87ba928e3259e10da8928e861ad951a8bb5aad9
gustavokenzo1 <gustavokenzo314@gmail.com>
2022-05-24 10:50:20 -0300

    remove feedget

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { FeedGet } from "./FeedGet/src/FeedGet";
 import { BrowserRouter as Router } from "react-router-dom";
 import DarkModeToggle from "./components/DarkModeToggle";
 import AnimatedRoutes from "./components/AnimatedRoutes";
-
-const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
   return (
     <>
       <Router>
         <DarkModeToggle />
-        <FeedGet apiKey={API_KEY!} />
         <AnimatedRoutes />
       </Router>
     </>
--- src/FeedGet/src/libs/api.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "https://nlw-return-impulse-production.up.railway.app/",
-});
